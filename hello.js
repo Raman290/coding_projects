2b3bb36be0d12c9589e86a35fddf022bb65527ce
Raman290 <74956357+Raman290@users.noreply.github.com>
2024-01-08 09:26:07 +0000

    adding new hello.js to project

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+const user = function (param) {
+    while(true){
+        if (param) {
+            continue;
+        } else {
+            break;
+        }
+    }
+}

be57d9b27137b1ec2406545c2587010d9a351213
Raman290 <74956357+Raman290@users.noreply.github.com>
2024-01-08 15:05:41 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,10 +1,14 @@
 console.log("hello world");
-const user = function (param) {
-    while(true){
-        if (param) {
-            continue;
-        } else {
+const user = function () {
+    console.log("Enter the number, which is even as well as prime.");
+    while (true) {
+        let number  = prompt("that number is : ");
+        if (number == 2) {
             break;
+        } else {
+            continue;
         }
     }
+    console.log("You guessed it correct.");
 }
+console.log(user());
